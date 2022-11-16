c0094e9fb2e75228cdb1f82e75faf0fab382478d
Wlauber <wlaubersamuel@gmail.com>
2022-11-16 17:46:08 -0300

    requisito 3 - getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees
+    .find((employees) =>
+      employees.firstName === employeeName
+      || employees.lastName === employeeName) || {};
 }
 
 module.exports = getEmployeeByName;
