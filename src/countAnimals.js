e1a81dd9024e8e7e71e5678ad10c9e01ffbfff2d
Wlauber <wlaubersamuel@gmail.com>
2022-11-18 19:54:45 -0300

    lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,26 +1,25 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-
 function countAnimals({ specie, sex } = {}) {
-    if (!specie) {
-        return data.species
-            .reduce((acc, curr) => {
-                acc[curr.name] = curr.residents.length;
-                return acc;
-            }, {});
-    }
+  if (!specie) {
+  return data.species
+    .reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
 
-    if (!sex) {
-        return data.species
-            .find((especie) => especie.name === specie).residents.length;
-    }
-    
-    if (!sex) {
-        return data.species.find((element) => element.name === specie).residents.length;
-      }
-      const especie = data.species.find((element) => element.name === specie);
-      return especie.residents.filter((resultFilter) => resultFilter.sex === sex).length;
+  if (!sex) {
+  return data.species
+    .find((especie) => especie.name === specie).residents.length;
+  }
+
+  if (!sex) {
+  return data.species.find((element) => element.name === specie).residents.length;
+  }
+  const especie = data.species.find((element) => element.name === specie);
+  return especie.residents.filter((resultFilter) => resultFilter.sex === sex).length;
 }
 
 module.exports = countAnimals;
