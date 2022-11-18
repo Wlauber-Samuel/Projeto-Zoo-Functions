4bcad074a85cfbec02e2901bafef380729e82551
Wlauber <wlaubersamuel@gmail.com>
2022-11-18 19:37:37 -0300

    Requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+
+function countAnimals({ specie, sex } = {}) {
+    if (!specie) {
+        return data.species
+            .reduce((acc, curr) => {
+                acc[curr.name] = curr.residents.length;
+                return acc;
+            }, {});
+    }
+
+    if (!sex) {
+        return data.species
+            .find((especie) => especie.name === specie).residents.length;
+    }
+    
+    if (!sex) {
+        return data.species.find((element) => element.name === specie).residents.length;
+      }
+      const especie = data.species.find((element) => element.name === specie);
+      return especie.residents.filter((resultFilter) => resultFilter.sex === sex).length;
 }
 
 module.exports = countAnimals;
