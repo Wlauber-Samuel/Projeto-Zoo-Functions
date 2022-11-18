3c104f25c77f14280d5fa6cc8cc9133c00a2d116
Wlauber <wlaubersamuel@gmail.com>
2022-11-18 20:15:54 -0300

    lint indeciso, agora vai 2.0!

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,21 +2,21 @@ const data = require('../data/zoo_data');
 
 function countAnimals({ specie, sex } = {}) {
   if (!specie) {
-  return data.species
-    .reduce((acc, curr) => {
-      acc[curr.name] = curr.residents.length;
+    return data.species
+      .reduce((acc, curr) => {
+        acc[curr.name] = curr.residents.length;
         return acc;
-        }, {});
-      }
+      }, {});
+  }
 
   if (!sex) {
-  return data.species
-    .find((especie) => especie.name === specie).residents.length;
-      }
+    return data.species
+      .find((especie) => especie.name === specie).residents.length;
+  }
 
   if (!sex) {
-  return data.species.find((element) => element.name === specie).residents.length;
-    }
+    return data.species.find((element) => element.name === specie).residents.length;
+  }
   const especie = data.species.find((element) => element.name === specie);
   return especie.residents.filter((resultFilter) => resultFilter.sex === sex).length;
 }
