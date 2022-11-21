d4f09b63bc1e4ad917d548cfb3b4a4be64f82db9
Wlauber <wlaubersamuel@gmail.com>
2022-11-21 15:22:57 -0300

    requisito 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -25,4 +25,19 @@ describe('Testes da função HandlerElephants', () => {
     expect(Array.isArray(received)).toEqual(true);
     expect(received).toEqual(expected);
   });
+
+  it ('Retorna "null" quando o parâmetro for diferente de "count, names, averageAge"', () => {
+      const received = handlerElephants('abc');
+      expect(received).toEqual(null);
+  }); 
+
+  it ('Retorna "undefined" quando não é passado nenhum parâmetro', () => {
+    const received = handlerElephants();
+    expect(received).toEqual(undefined);
+  }); 
+
+  it ('Retorna "Parâmetro inválido, é necessário uma string" quando é passado um parâmetro diferente de string', () => {
+    const received = handlerElephants(25);
+    expect(received).toBe('Parâmetro inválido, é necessário uma string')
+  });
 });
