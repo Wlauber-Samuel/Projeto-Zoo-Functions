6b2f84b9f50f12e87643f447d4b2dbafbf3221bb
Wlauber <wlaubersamuel@gmail.com>
2022-11-25 11:57:58 -0300

    .

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,7 +5,7 @@ const { species, employees } = data;
 function getOldestFromFirstSpecies(id) {
   const firstAnimal = employees.find((person) => person.id === id).responsibleFor[0];
   const animal = species.find((specie) => specie.id === firstAnimal).residents
-  .sort((a, b) => b.age - a.age).find((Oldest) => Oldest)
+    .sort((a, b) => b.age - a.age).find((Oldest) => Oldest);
   return Object.values(animal);
 }
 
