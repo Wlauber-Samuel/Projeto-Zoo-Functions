2b789d60f86f25dd4386f90c9b113ca05a9d3705
Wlauber <wlaubersamuel@gmail.com>
2022-11-25 08:50:08 -0300

    Requisito 9 - getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { hours, species } = data;
+const days = Object.keys(hours);
+const expectedObj = {};
+
+days.forEach((day) => {
+  expectedObj[day] = {
+    officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+    exhibition: species.filter(({ availability }) => availability
+      .includes(day)).map(({ name }) => name),
+  };
+});
+expectedObj.Monday = {
+  exhibition: 'The zoo will be closed!',
+  officeHour: 'CLOSED',
+};
+
+function getSchedule(ScheduleTarget) {
+  if (species.map(({ name }) => name).includes(ScheduleTarget)) {
+    return species.find(({ name }) => name === ScheduleTarget).availability;
+  }
+  if (days.includes(ScheduleTarget)) {
+    return ({ [ScheduleTarget]: expectedObj[ScheduleTarget] });
+  }
+  return expectedObj;
 }
 
 module.exports = getSchedule;
