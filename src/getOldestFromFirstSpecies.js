df0c82f7c9ad16910bb3b4dc8a4bae860bbe05e8
Wlauber <wlaubersamuel@gmail.com>
2022-11-25 10:29:41 -0300

    requisito 4 e 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = data.employees.find((person) => person.id === id).responsibleFor[0];
+  const animal = data.species.find((specie) => specie.id === firstAnimal).residents;
+  return Object.values(animal.sort((first, second) => second.age - first.age))[0];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((isMAnager) => isMAnager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else if (isManager(managerId)) {
+    const Manager = data.employees.filter((manager) => manager.managers.includes(managerId));
+    return Manager.map((name) => `${name.firstName} ${name.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
